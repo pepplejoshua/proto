6d28c9da3c42ab5e083b8bb1c83bd1bffccad75a
Caleb <CalebOttoGameDev@Gmail.com>
2022-06-05 12:08:11 +1000

    test: Shortened lexer token test

--- tests/main.rs
+++ tests/main.rs
@@ -6,9 +6,7 @@ fn test_lexer_tokens() -> Result<(), ProtoErr> {
 	let mut ctx = LexContext::new(fs::read_to_string("./examples/proto_tokens.pr").unwrap());
 
     loop {
-		let next = lex_next(&mut ctx)?;
-
-		if next.kind == TokenKind::End {
+		if lex_next(&mut ctx)?.kind == TokenKind::End {
 			break;
 		}
     }
