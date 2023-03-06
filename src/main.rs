2f79a736bcd49771c071abe9b703751b8481560a
Iwarilama <pepplejoshuatams@gmail.com>
2023-03-06 04:41:17 -0700

    Played around with a small example

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,18 @@ mod pastel;
 use pastel::pastel;
 
 fn main() {
+    /*
+    This example is akin to writing the following in html+css:
+        <div style="font-weight: bold; color: white; background-color: black;">
+            Welcome
+        </div>
+        <div style="font-weight: bold; color: white; background-color: cyan;">
+            To
+        </div>
+        <u style="color: white; background-color: magenta;">
+            Pastel!
+        </u>
+    */
     let text = r"
         *[*, l_white:d_black]Welcome[/]
         *[*, d_white:d_cyan]To[/] 
