7e4b610808a481ae99a9cfc386bed0a79a8de689
Joshua Pepple <pepplejoshuatams@gmail.com>
2024-05-22 21:31:54 -0600

    yolo

--- python/infer.py
+++ python/infer.py
@@ -26,6 +26,12 @@ class Constraint:
     def __str__(self) -> str:
         return f"Constraint[{self.left} = {self.right}]"
 
+# fn example(a i32, b) {
+#   let c = a + b;
+#   let d = c * 2;
+#   let e = d > 10;
+#   return e;
+# }
 FN_AST = [
     ("let", "a", Type("i32")),          # a: i32
     ("let", "b", TypeVariable("T_b")),  # b: unknown
@@ -80,6 +86,41 @@ def collect_constraints(ast):
                     case "gt":
                         CONSTRAINTS.append(Constraint(var_type, Type("bool")))
                         CONSTRAINTS.append(Constraint(left_type, right_type))
+        elif stmt[0] == "return":
+            return_expr = stmt[1]
+            return_type = TYPE_MAP.get(return_expr, TypeVariable(f"T_{return_expr}"))
+            CONSTRAINTS.append(Constraint(TypeVariable("T_return"), return_type))
+            TYPE_MAP["Inferred_Return_Type"] = return_type
+
+def unify(constraints):
+    solved = {}
+
+    # replaces all occurences of Var with Ty
+    def substitute(Var, Ty):
+        for constraint in constraints:
+            if constraint.left == Var:
+                constraint.left = Ty
+            if constraint.right == Var:
+                constraint.right = Ty
+
+    while constraints:
+        cons = constraints.pop(0)
+        left, right = cons.left, cons.right
+
+        # perform substitutions if required
+        if isinstance(left, TypeVariable):
+            if left not in solved:
+                solved[left] = right
+                # replace all occurences of left with right
+                substitute(left, right)
+        elif isinstance(right, TypeVariable):
+            if right not in solved:
+                solved[right] = left;
+                substitute(right, left)
+        elif left != right:
+            raise TypeError(f"cannot unify {left} and {right}")
+
+    return solved
 
 collect_constraints(FN_AST)
 
@@ -91,3 +132,10 @@ print()
 print("[CONSTRAINTS]")
 for constraint in CONSTRAINTS:
     print(f"  - {constraint}")
+print()
+
+# will contain the solved types from the TYPE_MAP and CONSTRAINTS
+solved = unify(CONSTRAINTS)
+print("[SOLVED]")
+for Left, Right in solved.items():
+    print(f"  - {Left} => {Right}")
