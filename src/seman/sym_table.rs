7edbb65f3c7146450c04016507c9b2213808b50d
Joshua Pepple <pepplejoshuatams@gmail.com>
2024-09-07 22:46:41 -0600

    "Work on SymbolTable nesting, to support splintered checking at different points of the symbol table tree"

--- src/seman/sym_table.rs
+++ src/seman/sym_table.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use std::{collections::HashMap, rc::Rc};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use crate::{parser::ast::Ins, source::source::SourceRef};
 
@@ -16,63 +16,74 @@ pub enum SymbolInfo {
     Unresolved {
         ins: Ins,
         mutable: bool,
-    }
+    },
 }
 
 #[derive(Debug, Clone)]
 pub struct SymbolScope {
-    pub parent: Option<Rc<SymbolScope>>,
+    pub parent: Option<Rc<RefCell<SymbolScope>>>,
     pub names: HashMap<Rc<String>, SymbolInfo>,
 }
 
 impl SymbolScope {
-    pub fn find(&self, name: &String) -> Option<&SymbolInfo> {
-        self.names.get(name)
+    pub fn new(parent: Option<Rc<RefCell<SymbolScope>>>) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(SymbolScope {
+            parent,
+            names: HashMap::new(),
+        }))
     }
 
-    pub fn enter_scope(&mut self)  -> Rc<SymbolScope> {
-        Rc::new(SymbolScope {
-            parent: Some(self),
-            names: HashMap::new(),
-        });
+    pub fn shallow_lookup(&self, name: &String) -> Option<SymbolInfo> {
+        self.names.get(name).cloned()
+    }
+
+    pub fn deep_lookup(&self, name: &String) -> Option<SymbolInfo> {
+        self.names.get(name).cloned().or_else(|| {
+            self.parent
+                .as_ref()
+                .and_then(|p| p.borrow().deep_lookup(name))
+        })
+    }
+
+    pub fn insert(&mut self, name: Rc<String>, info: SymbolInfo) {
+        self.names.insert(name, info);
+    }
+
+    pub fn create_child(&self) -> Rc<RefCell<SymbolScope>> {
+        SymbolScope::new(Some(Rc::new(RefCell::new(self.clone()))))
     }
 }
 
 pub struct SymbolTable {
-    pub cur_scope: Rc<SymbolScope>,
+    pub current_scope: Rc<RefCell<SymbolScope>>,
 }
 
 impl SymbolTable {
     pub fn new() -> SymbolTable {
         SymbolTable {
-            cur_scope: Rc::new(SymbolScope {
-                names: HashMap::new(),
-                parent: None,
-            })
+            current_scope: SymbolScope::new(None),
         }
     }
 
-    pub fn ente_ood_scope() -> SymbolTable {
-        self.scopes.push(SymbolScope::)
+    pub fn enter_scope(&mut self) {
+        let new_scope = self.current_scope.borrow().create_child();
+        self.current_scope = new_scope;
     }
 
     pub fn exit_scope(&mut self) {
-        if self.scopes.len() > 1 {
-            self.scopes.pop();
+        if let Some(parent) = self.current_scope.clone().borrow().parent.clone() {
+            self.current_scope = parent;
+        } else {
+            // Handle error: trying to exit the global scope
+            panic!("Attempting to exit the global scope");
         }
     }
 
-    pub fn shallow_find_symbol_info(&self, name: &String) -> Option<&SymbolInfo> {
-        self.scopes.last().unwrap().find(name)
+    pub fn shallow_find_symbol_info(&self, name: &String) -> Option<SymbolInfo> {
+        self.current_scope.borrow().shallow_lookup(name)
     }
 
-    pub fn find_symbol_info(&self, name: &String) -> Option<&SymbolInfo> {
-        for scope in self.scopes.iter().rev() {
-            let symbol_info = scope.find(name);
-            if symbol_info.is_some() {
-                return symbol_info;
-            }
-        }
-        None
+    pub fn deep_find_symbol_info(&self, name: &String) -> Option<SymbolInfo> {
+        self.current_scope.borrow().deep_lookup(name)
     }
 }
